6455b4d1ff3a529d02edfbbeeed764508ded4de4
Sanjeev Yadav <sanjeevy133@protonmail.com>
2022-10-14 21:12:18 +0530

    feat: Add navigation component and dummy login, signup fragment

---
+++ app/src/main/java/com/sanjeevyadavit/magecart/ui/LoginFragment.kt
@@ -0,0 +1,33 @@
+package com.sanjeevyadavit.magecart.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.sanjeevyadavit.magecart.R
+import com.sanjeevyadavit.magecart.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding =  DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.signupCta.setOnClickListener {
+            val action = LoginFragmentDirections.actionLoginFragmentToSignupFragment()
+            it.findNavController().navigate(action)
+        }
+    }
+}
