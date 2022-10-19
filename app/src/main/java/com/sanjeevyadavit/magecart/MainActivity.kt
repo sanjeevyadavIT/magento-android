5181262e5a296dd59431e5c8e95bca75f1cbb1d3
Sanjeev Yadav <sanjeevy133@protonmail.com>
2022-10-19 20:17:08 +0530

    feat: Attach Toolbar with navigation

--- app/src/main/java/com/sanjeevyadavit/magecart/MainActivity.kt
+++ app/src/main/java/com/sanjeevyadavit/magecart/MainActivity.kt
@@ -3,6 +3,10 @@ package com.sanjeevyadavit.magecart
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.sanjeevyadavit.magecart.viewmodel.MainActivityViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +16,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
     }
 }
