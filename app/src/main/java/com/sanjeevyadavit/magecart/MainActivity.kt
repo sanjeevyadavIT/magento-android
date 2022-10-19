7265c229503803cad1942167e12d77338ca3bbab
Sanjeev Yadav <sanjeevy133@protonmail.com>
2022-10-19 23:25:34 +0530

    feat: Setup Drawer Navigation

--- app/src/main/java/com/sanjeevyadavit/magecart/MainActivity.kt
+++ app/src/main/java/com/sanjeevyadavit/magecart/MainActivity.kt
@@ -4,27 +4,41 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import com.sanjeevyadavit.magecart.databinding.ActivityMainBinding
 import com.sanjeevyadavit.magecart.viewmodel.MainActivityViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: MainActivityViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setupToolbar()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        getNavController().let {
+            setupToolbar(it)
+            setupDrawer(it)
+        }
     }
 
-    private fun setupToolbar() {
+    private fun getNavController(): NavController {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-        val navController = navHostFragment.navController
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
-        findViewById<Toolbar>(R.id.toolbar)
-            .setupWithNavController(navController, appBarConfiguration)
+        return navHostFragment.navController
+    }
+
+    private fun setupToolbar(navController: NavController) {
+        val appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerContainer)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
+    }
+
+    private fun setupDrawer(navController: NavController) {
+        binding.drawerView.setupWithNavController(navController)
     }
 }
